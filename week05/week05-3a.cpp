45982ffbec6f80253bed875705e3569389fabd9b
Youminnnnn <13750625@me.mcu.edu.tw>
2025-10-08 11:41:24 +0800

    week05

---
+++ week05/week05-3a.cpp
@@ -0,0 +1,16 @@
+/// week05-3a.cpp 三合一 Part1/Part2 Input/Output
+/// CPE 這次的第2題 UVA 483 Word Scramble 把字弄反
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+int main()
+{
+    string line; /// Part1: Input
+    while(getline(cin, line)){
+    /// 讀入1行, 讀到Ctrl-Z才結束
+
+        /// Part 2: Output
+        cout << line << endl; ///先隨便印出來
+    }
+}
